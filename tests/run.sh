3d7f92d76de074d813ae8569deeaf6cb16a333f5
Mallory Adams <mallory@dxw.com>
2020-10-29 09:07:39 -0400

    Add passing write/destroy/read test

--- tests/run.sh
+++ tests/run.sh
@@ -23,6 +23,13 @@ run_tests() {
   echo "✅ write/read"
 
   reset_cookies
+  fetch "write.php?handler=$HANDLER&key=animal&value=dog"
+  fetch "destroy.php?handler=$HANDLER"
+  ANIMAL=$(fetch "read.php?handler=$HANDLER&key=animal")
+  test "${ANIMAL}" = ""
+  echo "✅ write/destroy/read"
+
+  reset_cookies
   PASS=$(fetch "constructor-should-throw-exception.php?handler=$HANDLER")
   test "$PASS" = PASS
   echo "✅ constructor-should-throw-exception.php"
