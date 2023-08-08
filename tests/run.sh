00bf449834b10f5394781436a3ec8cf06d6a3bb4
Rob Skilling <rob@dxw.com>
2023-08-08 15:15:20 +0100

    Run integration tests in PHP 7.4, 8.1 & 8.2
    This has required a minor amendment to `tests/www/read.php` because
    trying to get a non-existent array key threw an error in PHP 8.

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,8 @@ set -e
 COOKIES=/tmp/cookies
 
 fetch() {
-  curl --silent --show-error --cookie-jar "$COOKIES" --cookie "$COOKIES" "http://php/tests/www/$1"
+  PHP_VERSION_NO_DOT=$(echo "$2" | sed "s/\.//")
+  curl --silent --show-error --cookie-jar "$COOKIES" --cookie "$COOKIES" "http://php$PHP_VERSION_NO_DOT/tests/www/$1"
 }
 
 reset_cookies() {
@@ -13,42 +14,48 @@ reset_cookies() {
 
 run_tests() {
   HANDLER=$1
+  PHP_VERSION=$2
 
-  echo "Running tests using '$HANDLER' handler..."
+  echo "Running tests using '$HANDLER' handler, PHP Version $PHP_VERSION..."
 
   reset_cookies
-  fetch "write.php?handler=$HANDLER&key=animal&value=dog"
-  ANIMAL=$(fetch "read.php?handler=$HANDLER&key=animal")
+  fetch "write.php?handler=$HANDLER&key=animal&value=dog" $PHP_VERSION
+  ANIMAL=$(fetch "read.php?handler=$HANDLER&key=animal" $PHP_VERSION)
   test "${ANIMAL}" = dog
   echo "✅ write/read"
 
   reset_cookies
-  fetch "write.php?handler=$HANDLER&key=animal&value=dog"
-  fetch "destroy.php?handler=$HANDLER"
-  ANIMAL=$(fetch "read.php?handler=$HANDLER&key=animal")
+  fetch "write.php?handler=$HANDLER&key=animal&value=dog" $PHP_VERSION
+  fetch "destroy.php?handler=$HANDLER" $PHP_VERSION
+  ANIMAL=$(fetch "read.php?handler=$HANDLER&key=animal" $PHP_VERSION)
   test "${ANIMAL}" = ""
   echo "✅ write/destroy/read"
 
   reset_cookies
-  fetch "write.php?handler=$HANDLER&key=animal&value=dog"
-  fetch "unset.php?handler=$HANDLER&key=animal"
-  ANIMAL=$(fetch "read.php?handler=$HANDLER&key=animal")
+  fetch "write.php?handler=$HANDLER&key=animal&value=dog" $PHP_VERSION
+  fetch "unset.php?handler=$HANDLER&key=animal" $PHP_VERSION
+  ANIMAL=$(fetch "read.php?handler=$HANDLER&key=animal" $PHP_VERSION)
   test "${ANIMAL}" = ""
   echo "✅ write/unset/read"
 
   reset_cookies
-  PASS=$(fetch "constructor-should-throw-exception.php?handler=$HANDLER")
+  PASS=$(fetch "constructor-should-throw-exception.php?handler=$HANDLER" $PHP_VERSION)
   test "$PASS" = PASS
   echo "✅ constructor-should-throw-exception.php"
 
   reset_cookies
-  ID=$(fetch "id.php?handler=$HANDLER")
+  ID=$(fetch "id.php?handler=$HANDLER" $PHP_VERSION)
   PASS=$(echo "$ID" | perl -pe 's/^[0-9a-f]{32}$/PASS/')
   test "$PASS" = PASS
   echo "✅ id"
 
-  echo "All tests pass for '$HANDLER' handler!"
+  echo "All tests pass for '$HANDLER' handler on PHP $PHP_VERSION!"
 }
 
-run_tests php
-run_tests redis
+run_tests redis 7.4
+run_tests php 7.4
+run_tests redis 8.1
+run_tests php 8.1
+run_tests redis 8.2
+run_tests php 8.2
+
--- tests/www/read.php
+++ tests/www/read.php
@@ -3,4 +3,8 @@
 require_once __DIR__.'/common.php';
 
 session_start();
-echo $_SESSION[$_GET['key']];
+if (isset($_SESSION[$_GET['key']])) {
+	echo $_SESSION[$_GET['key']];
+} else {
+	echo "";
+}
